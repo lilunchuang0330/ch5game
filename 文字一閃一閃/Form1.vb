92ba4c0e29efde76cac580c11dee8d9acbb2a56c
lilunchuang0330 <alan20030330@gmail.com>
2022-12-13 20:52:39 +0800

    1213

---
+++ 文字一閃一閃/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1_Timer()
+    End Sub
+
+
+
+    Private Sub Timer1_Timer()
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Label1.ForeColor = ForeColor.Red Then
+
+
+
+            Label1.ForeColor = ForeColor.Transparent
+
+
+
+        Else
+
+
+
+            Label1.ForeColor = ForeColor.Red
+
+
+
+        End If
+    End Sub
+End Class
